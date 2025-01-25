78721ed082cf1393132b6ed54cd80c4c62219cf6
Mulju <samuli.haltimo@metropolia.fi>
2025-01-25 17:26:18 +0200

    Audio system changes

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,9 +1,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager Instance { get; private set; }
+
+    public AudioClip[] jumpClips;
+    public AudioClip[] shootClips;
+    public AudioClip[] ghostClips;
+    public AudioClip[] deathClips;
+    public AudioClip[] bubbleBounceClips;
+    public AudioClip[] scoreClips;
+    public AudioClip[] plopClips;
+
     /// <summary>
     /// Reference to the audio mixer.
     /// </summary>
@@ -19,6 +31,21 @@ public class AudioManager : MonoBehaviour
     /// </summary>
     public bool variatePitch = true;
 
+    /// <summary>
+    /// Singleton initialization
+    /// </summary>
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    
     /// <summary>
     /// Modifies master volume based on slider value change.
     /// </summary>
@@ -50,7 +77,7 @@ public class AudioManager : MonoBehaviour
     /// Plays a sound audio clip.
     /// </summary>
     /// <param name="clip">Audio clip to be played.</param>
-    public void PlaySound(AudioClip clip)
+    private void PlaySound(AudioClip clip)
     {
         foreach(AudioSource source in soundPool)
         {
@@ -70,6 +97,12 @@ public class AudioManager : MonoBehaviour
         audioSource.PlayOneShot(clip);
     }
 
+    private void PlayRandomSound(AudioClip[] clips)
+    {
+        int index = Random.Range(0, clips.Length);
+        PlaySound(clips[index]);
+    }
+
     /// <summary>
     /// Variates the pitch for an audio source.
     /// </summary>
@@ -77,5 +110,35 @@ public class AudioManager : MonoBehaviour
     private float VariatePitch()
     {
         return Random.Range(0.9f, 1.1f);
+    }
+
+    public void PlaySoundFromAnimationEvent(string arrayName)
+    {
+        switch (arrayName)
+        {
+            case "Jump":
+                PlayRandomSound(jumpClips);
+                break;
+            case "Shoot":
+                PlayRandomSound(shootClips);
+                break;
+            case "Ghost":
+                PlayRandomSound(ghostClips);
+                break;
+            case "Death":
+                PlayRandomSound(deathClips);
+                break;
+            case "BubbleBounce":
+                PlayRandomSound(bubbleBounceClips);
+                break;
+            case "Score":
+                PlayRandomSound(scoreClips);
+                break;
+            case "Plop":
+                PlayRandomSound(plopClips);
+                break;
+            default:
+                break;
+        }
     }
 }
