f1241df58ca3d47ac5ad9747aa9079da4ace05a3
Mulju <samuli.haltimo@metropolia.fi>
2025-01-25 17:38:57 +0200

    Audiomanager prefab

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -7,6 +7,13 @@ using UnityEngine.UIElements;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance { get; private set; }
+#if UNITY_EDITOR
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    static void Init()
+    {
+        Instance = null;
+    }
+#endif
 
     public AudioClip[] jumpClips;
     public AudioClip[] shootClips;
@@ -24,12 +31,18 @@ public class AudioManager : MonoBehaviour
     /// <summary>
     /// A pool of audio sources.
     /// </summary>
-    public List<AudioSource> soundPool = new List<AudioSource>();
+    [ContextMenuItem("Test audio source", "TestAudio")]
+    public List<AudioSource> soundSourcePool = new List<AudioSource>();
     
     /// <summary>
     /// Enables pitch variation for sounds.
     /// </summary>
     public bool variatePitch = true;
+
+    public AudioMixerGroup masterGroup;
+    public AudioMixerGroup musicGroup;
+    public AudioMixerGroup soundGroup;
+
 
     /// <summary>
     /// Singleton initialization
@@ -43,6 +56,7 @@ public class AudioManager : MonoBehaviour
         else
         {
             Instance = this;
+            DontDestroyOnLoad(this.gameObject);
         }
     }
     
@@ -79,7 +93,7 @@ public class AudioManager : MonoBehaviour
     /// <param name="clip">Audio clip to be played.</param>
     private void PlaySound(AudioClip clip)
     {
-        foreach(AudioSource source in soundPool)
+        foreach(AudioSource source in soundSourcePool)
         {
             // If a source is not playing, use it and then exit the function
             if(!source.isPlaying)
@@ -92,7 +106,8 @@ public class AudioManager : MonoBehaviour
 
         // No unused audio source was found, so create a new one
         AudioSource audioSource = gameObject.AddComponent<AudioSource>();
-        soundPool.Add(audioSource);
+        soundSourcePool.Add(audioSource);
+        audioSource.outputAudioMixerGroup = soundGroup;
         audioSource.pitch = variatePitch ? VariatePitch() : 1;
         audioSource.PlayOneShot(clip);
     }
@@ -140,5 +155,11 @@ public class AudioManager : MonoBehaviour
             default:
                 break;
         }
+    }
+
+    // Test function
+    private void TestAudio()
+    {
+        PlayRandomSound(jumpClips);
     }
 }
