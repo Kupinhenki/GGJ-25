39085ef3cf970338ea69cbc4d0a694b770e5b5f4
villev00 <91452512+villev00@users.noreply.github.com>
2025-01-26 11:52:02 +0200

    more animations

--- Assets/Scripts/Bubble/Shoot.cs
+++ Assets/Scripts/Bubble/Shoot.cs
@@ -8,15 +8,18 @@ public class Shoot : MonoBehaviour
     [SerializeField] private GameObject bubble;
     [SerializeField] private float minimumShootCooldown = 1f;
     [SerializeField] private bool canShoot = true;
+    [SerializeField] private Animator animator;
     public void ShootBubble()
     {
         if (!movement.isGhostMode && bubble == null && movement.isActiveAndEnabled)
         {
             canShoot = false;
+            animator.SetTrigger("Shoot");
             bubble = Instantiate(bubblePrefab, new Vector2(bubbleSpawnPoint.position.x, bubbleSpawnPoint.position.y), bubbleSpawnPoint.rotation);
             Vector2 direction = movement.IsFacingRight ? Vector2.right : Vector2.left;
             Bubble bubbleScript = bubble.GetComponent<Bubble>();
             bubbleScript.ShootBubble(direction);
+            
             AudioManager.Instance.PlaySoundFromAnimationEvent("Shoot");
             Invoke("SetCanShootTrue", minimumShootCooldown);
         }
